4a525c12fcd121e6f29b26412ee701102c26331c
Eli <elionlink@gmail.com>
2020-08-19 00:12:05 +0300

    180820

--- src/Services/ApiService.js
+++ src/Services/ApiService.js
@@ -1,19 +1,19 @@
 export const apiSearchAll = async (input) => {
-  const url = `http://www.omdbapi.com/${input}&apikey=70630e18`;
+  const url = `https://www.omdbapi.com/${input}&apikey=70630e18`;
   let resp = await fetch(url);
   let data = await resp.json();
   return data;
 };
 
 export const apiGetSingleShort = async (id) => {
-  const url = `http://www.omdbapi.com/?i=${id}&apikey=70630e18`;
+  const url = `https://www.omdbapi.com/?i=${id}&apikey=70630e18`;
   let resp = await fetch(url);
   let data = await resp.json();
   return data;
 };
 
 export const apiGetSingleLong = async (id) => {
-  const url = `http://www.omdbapi.com/?i=${id}&plot=full&apikey=70630e18`;
+  const url = `https://www.omdbapi.com/?i=${id}&plot=full&apikey=70630e18`;
   let resp = await fetch(url);
   let data = await resp.json();
   return data;
